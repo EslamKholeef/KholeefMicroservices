e40e5571491c44a39f7ff939db0544cc381d89d7
EslamKholeef-GS <Eslamk@GSTechnologyCo.com>
2025-07-15 00:37:07 +0300

    Order Service: Infrastructure With Migrations

---
+++ Order.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Domain.Entities;
+
+namespace Order.Infrastructure.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<OrderModel> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderModel>()
+                .HasMany(o => o.Items)
+                .WithOne()
+                .HasForeignKey(oi => oi.Id);
+        }
+    }
+}
